6af7b736d5213b789451b37830d627c29f04ad22
zyro <xzyro7@gmail.com>
2019-08-18 18:37:01 +0200

    backend

---
+++ config/keys.js
@@ -0,0 +1,3 @@
+module.exports = {
+    mongoURI: 'mongodb+srv://zyro:patata4321@tattoo-lzg1z.mongodb.net/test?retryWrites=true&w=majority'
+}
---
+++ server.js
@@ -0,0 +1,25 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
+
+const items = require('./routes/api/Items');
+
+const app = express();
+
+// BodyParser Middleware
+app.use(bodyParser.json());
+
+// DB Config
+const db = require('./config/keys').mongoURI;
+
+//Connect to mongo
+mongoose.connect(db)
+    .then(() => console.log('mongoDB connected...'))
+    .catch(err => console.log(err));
+
+// Use routes
+app.use('/api/items', items);
+
+const port = process.env.PORT || 5000;
+
+app.listen(port, () => console.log(`Server started on port ${port}`));
